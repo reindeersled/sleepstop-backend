3350f7da2d22f8b1a6d44b00708fc20bc259345e
luckyfish-o <trickortreat476@gmail.com>
2025-06-04 11:32:45 -0400

    Confirmed all model and index.js changes for deployment

--- models/index.js
+++ models/index.js
@@ -1,38 +1,27 @@
-// sleepstop-backend/models/index.js (UPDATED)
-
 const { Sequelize } = require('sequelize');
 
-// Use DATABASE_URL provided by Heroku
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres', // Heroku Postgres is always 'postgres'
+  dialect: 'postgres',
   protocol: 'postgres',
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false // This is important for Heroku
+      rejectUnauthorized: false
     }
   },
-  logging: false // Optional: set to true to see SQL queries in logs
+  logging: false
 });
 
 const models = {
-  User: require('./User')(sequelize, Sequelize),
-  Alarm: require('./Alarm')(sequelize, Sequelize)
+  User: require('./User')(sequelize, Sequelize), // This *must* return a model
+  Alarm: require('./Alarm')(sequelize, Sequelize) // This *must* return a model
 };
 
-// Associate models if association exists
-// This loop is correct for your 'models' object
 Object.keys(models).forEach(modelName => {
   if (models[modelName].associate) {
-    models[modelName].associate(models); // Pass the entire 'models' object for associations
+    models[modelName].associate(models);
   }
 });
-
-// Optional: Sync your models (create tables)
-// sequelize.sync()
-//   .then(() => console.log('Database synced!'))
-//   .catch(err => console.error('Database sync error:', err));
-
 
 module.exports = {
   ...models,
